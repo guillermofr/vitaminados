ac3fc21636d539bb2c542dfe4c07532610b0c151
Guillermo Fernández <guillermofr@digio.es>
2013-10-09 21:36:45 +0200

    aniadido ejemplo con recaptcha y areyouahuman

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -26,9 +26,105 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		//change this code after install
-		
 		$this->load->spark('twiggy/0.8.5');
-		$this->twiggy->display('welcome');
+
+		if ($_POST){
+
+			$consecutivos = $this->input->post('consecutivos');
+
+			switch($this->input->post('type')){
+
+				case 'recaptcha':
+
+					$this->load->helper('recaptcha/recaptchalib');
+					//require_once('recaptchalib.php'); // reCAPTCHA Library
+					$privkey = "6Le4k-gSAAAAANIdExCmaoHeBPU4-mb1baMElBQc"; // Private API Key
+					$verify = recaptcha_check_answer($privkey, $_SERVER['REMOTE_ADDR'], $_POST['recaptcha_challenge_field'], $_POST['recaptcha_response_field']);
+
+					if ($verify->is_valid) {
+					  # Enter Success Code
+					  echo "Your response was correct!";
+					  $consecutivos = $consecutivos +1;
+					}
+					else {
+					  # Enter Failure Code
+					  echo "You did not enter the correct words.  Please try again.";
+					  $consecutivos = 0;
+					}
+
+
+				break;
+
+				case 'ayah':
+
+					$this->load->helper('ayah/ayah');
+					//require_once("ayah.php");
+					$ayah = new AYAH();
+					// Check to see if the user has submitted the form. You will need to replace
+					// 'my_submit_button_name' with the name of your 'Submit' button.
+					
+			        // Use the AYAH object to see if the user passed or failed the game.
+			        $score = $ayah->scoreResult();
+			        if ($score)
+			        {
+			            // This happens if the user passes the game. In this case,
+			            // we're just displaying a congratulatory message.
+			                echo "Congratulations: you are a human!";
+			                $consecutivos = $consecutivos +1;
+			        }
+			        else
+			        {
+			            // This happens if the user does not pass the game.
+			            echo "Sorry, but we were not able to verify you as human. Please try again.";
+			            $consecutivos = 0;
+			        }
+					
+
+
+				break;
+				default:
+					echo "hacker!";
+					$consecutivos = 0;
+				break;
+			}
+
+		} else {
+			$consecutivos = 0;
+		}
+
+
+		if ($consecutivos%2){
+			$type = 'recaptcha';
+		} else {
+			$type = 'ayah';
+		}
+			
+
+		switch ($type){
+
+			case 'recaptcha':
+
+				$data = array('consecutivos' => $consecutivos,'type' => 'recaptcha'); 
+				$this->twiggy->set($data);
+				$this->twiggy->display('captchas/recaptcha',$data);
+			break;
+
+			case 'ayah': 
+				$this->load->helper('ayah/ayah');
+					//require_once("ayah.php");
+				$ayah = new AYAH();
+				$data = array('consecutivos' => $consecutivos,'type' => 'ayah','ayah_game'=> $ayah->getPublisherHTML());
+				$this->twiggy->set($data);
+				$this->twiggy->display('captchas/ayah',$data);
+			break;
+
+		}
+
+
+		
+
+		
+
 		
 	}
 	
---
+++ application/helpers/ayah/ayah_config.php
@@ -0,0 +1,13 @@
+<?php
+// Edit the two lines below to use the keys for your site.
+// (Note: you can find your keys at http://portal.areyouahuman.com/dashboard)
+define( 'AYAH_PUBLISHER_KEY', '760cf621ea51419cda8a2734ab69dd9bbd9f5a82');
+define( 'AYAH_SCORING_KEY', 'eafd10d91080d6e718f814fb21c89b66d4b84a85');
+
+
+// Set defaults for values needed by the ayah.php file.
+// (Note: you do not need to change these.)
+define( 'AYAH_WEB_SERVICE_HOST', 'ws.areyouahuman.com');
+define( 'AYAH_TIMEOUT', 0);
+define( 'AYAH_DEBUG_MODE', FALSE);
+define( 'AYAH_USE_CURL', TRUE);
