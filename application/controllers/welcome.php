b9cac8c6e3d29f783eb983247fcd7ba20514cc44
Guillermo Fernández <guillermofr@digio.es>
2013-10-13 13:58:37 +0200

    reordenando

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -25,105 +25,80 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
+
+		$num_games = 2;
+
+
 		//change this code after install
 		$this->load->spark('twiggy/0.8.5');
 
+		//checkeo del captcha 
 		if ($_POST){
 
 			$consecutivos = $this->input->post('consecutivos');
 
+			$captchacorrecto = false;
+
+			//segun el type lo comprobaremos de una forma u otra, estos check solo tienen
+			//que darle un valor a captchacorrecto (true/false)
 			switch($this->input->post('type')){
-
 				case 'recaptcha':
-
-					$this->load->helper('recaptcha/recaptchalib');
-					//require_once('recaptchalib.php'); // reCAPTCHA Library
-					$privkey = "6Le4k-gSAAAAANIdExCmaoHeBPU4-mb1baMElBQc"; // Private API Key
-					$verify = recaptcha_check_answer($privkey, $_SERVER['REMOTE_ADDR'], $_POST['recaptcha_challenge_field'], $_POST['recaptcha_response_field']);
-
-					if ($verify->is_valid) {
-					  # Enter Success Code
-					  echo "Your response was correct!";
-					  $consecutivos = $consecutivos +1;
-					}
-					else {
-					  # Enter Failure Code
-					  echo "You did not enter the correct words.  Please try again.";
-					  $consecutivos = 0;
-					}
-
-
+					include('includes/recaptcha_check.php');
 				break;
-
 				case 'ayah':
-
-					$this->load->helper('ayah/ayah');
-					//require_once("ayah.php");
-					$ayah = new AYAH();
-					// Check to see if the user has submitted the form. You will need to replace
-					// 'my_submit_button_name' with the name of your 'Submit' button.
-					
-			        // Use the AYAH object to see if the user passed or failed the game.
-			        $score = $ayah->scoreResult();
-			        if ($score)
-			        {
-			            // This happens if the user passes the game. In this case,
-			            // we're just displaying a congratulatory message.
-			                echo "Congratulations: you are a human!";
-			                $consecutivos = $consecutivos +1;
-			        }
-			        else
-			        {
-			            // This happens if the user does not pass the game.
-			            echo "Sorry, but we were not able to verify you as human. Please try again.";
-			            $consecutivos = 0;
-			        }
-					
-
-
+					include('includes/ayah_check.php');
 				break;
 				default:
-					echo "hacker!";
+					// hackerrr!!
 					$consecutivos = 0;
 				break;
 			}
 
+			//acción por defecto al rellenar captchas
+			if ($captchacorrecto) {
+				//TODO contabilizar combos y adjudicar vitaminas nuevas
+				$consecutivos++;
+			} else {
+				//seteamos a 0 el combo y no damos una mierda
+				$consecutivos=0;
+			}
+
 		} else {
+			//primera carga de la web
 			$consecutivos = 0;
 		}
 
 
-		if ($consecutivos%2){
-			$type = 'recaptcha';
-		} else {
-			$type = 'ayah';
+		// selector de juego que aparecerá en el siguiente turno
+		switch($consecutivos%$num_games) {
+			case 0:
+				$type = 'recaptcha';
+			break;
+			case 1:
+				$type = 'ayah';
+			break;
+			//añadir aquí resto de juegos para que aparezcan
+
+			default:
+				$type = 'recaptcha';
+			break;
 		}
-			
 
 		switch ($type){
-
 			case 'recaptcha':
-
 				$data = array('consecutivos' => $consecutivos,'type' => 'recaptcha'); 
-				$this->twiggy->set($data);
-				$this->twiggy->display('captchas/recaptcha',$data);
 			break;
 
 			case 'ayah': 
 				$this->load->helper('ayah/ayah');
-					//require_once("ayah.php");
 				$ayah = new AYAH();
 				$data = array('consecutivos' => $consecutivos,'type' => 'ayah','ayah_game'=> $ayah->getPublisherHTML());
-				$this->twiggy->set($data);
-				$this->twiggy->display('captchas/ayah',$data);
 			break;
-
 		}
 
 
-		
-
-		
+		$this->twiggy->set($data);
+		$this->twiggy->display('captchas/'.$type,$data);
 
 		
 	}
