008387b2cce9d046f1b60b2021c606d67d7052a7
Guillermo Fernández <guillermofr@digio.es>
2013-11-09 13:59:49 +0100

    mas info en la vitamina de ejemplo

--- application/models/vitaminas/ejemplo.php
+++ application/models/vitaminas/ejemplo.php
@@ -1,6 +1,12 @@
 <?php
 /* Fichero de ejemplo de vitamina */
 
+/*
+	Parametros disponibles
+		$target_id 				-> id del usuario objetivo
+		$CI->bitauth->user_id 	-> id del usuario logueado
+*/
+
 //obtener datos del usuario logueado
 
 		$own = $CI->bitauth->get_user_by_id($CI->bitauth->user_id);
