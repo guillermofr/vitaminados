864802ace38446c195cf7d142219397786ac84d7
Guillermo Fernández <guillermofr@digio.es>
2013-11-29 00:11:49 +0100

    estilo

--- application/db/vitaminas12112013.sql
+++ application/db/vitaminas12112013.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50524
 File Encoding         : 65001
 
-Date: 2013-11-12 22:01:08
+Date: 2013-11-29 00:01:33
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,7 +27,7 @@ CREATE TABLE `vitamina` (
   `categoria` int(11) DEFAULT NULL,
   `fichero` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of vitamina
@@ -53,3 +53,13 @@ INSERT INTO `vitamina` VALUES ('18', 'Avanza!', '<p>Suma 2 combos a la racha del
 INSERT INTO `vitamina` VALUES ('19', 'Avanza más!', '<p>Suma 5 combos a la racha del objetivo</p>', '100', '3', 'RachaSumar5.php');
 INSERT INTO `vitamina` VALUES ('20', 'Ruleta rusa!', '<p>50% de posibilidades de poner a 0 los puntos del objetivo... cuidado! que si falla te pones a 0 tu!!!</p>', '30', '1', 'RuletaRusa.php');
 INSERT INTO `vitamina` VALUES ('21', 'Vamos a medias...', '<p>Iguala tu puntuación y la del objetivo a la cantidad media de los dos</p>', '30', '2', 'Salomon.php');
+INSERT INTO `vitamina` VALUES ('22', 'Te cambio las vitaminas!', '<p>Intercambia todas tus vitaminas por las de tu objetivo</p>', '20', '1', 'IntercambioVitaminas.php');
+INSERT INTO `vitamina` VALUES ('23', 'Te quito todo', '<p>Borra todas las vitaminas de tu adversario!</p>', '20', '1', 'BorraVitaminas.php');
+INSERT INTO `vitamina` VALUES ('24', 'Te borro una vitamina', '<p>Borra una vitamia de tu adversario</p>', '30', '2', 'Borra1Vitamina.php');
+INSERT INTO `vitamina` VALUES ('25', 'Te borro dos vitaminas!', '<p>Borra dos vitaminas de tu adversario!</p>', '30', '2', 'Borra2Vitaminas.php');
+INSERT INTO `vitamina` VALUES ('26', 'Toma un rosco!', '<p>Si ves que alguien está haciendo un combo muy alto, cortale el rollo y que se coma un rosco!, ponle el combo a 0 y que empiece de nuevo XD</p>', '120', '3', 'RachaResetea.php');
+INSERT INTO `vitamina` VALUES ('27', 'Rachea el doble!', '<p>Multiplica por 2 el combo del objetivo! </p>', '120', '3', 'RachaMultiplica2.php');
+INSERT INTO `vitamina` VALUES ('28', 'Divide y perderás', '<p>Divide por 2 la cantidad de combos del jugador seleccionado</p>', '120', '3', 'RachaDivide2.php');
+INSERT INTO `vitamina` VALUES ('29', 'El karma', '<p>El karma siempre te lo devuelve, esta vitamina hace que el jugador seleccionado sufra el efecto de la última vitamina que haya utilizado contra alguien, aplicándoselo a él mismo</p>', '40', '3', 'KarmaRepite.php');
+INSERT INTO `vitamina` VALUES ('30', 'OMG', '<p>Hoy te sientes generoso y optas por donar a cualquier persona todos tus puntos, ¿por què? porque sí.</p>', '30', '3', 'ONG.php');
+INSERT INTO `vitamina` VALUES ('31', 'Salvaculos', '<p>Esta vitamina es una de las más importantes estratégicamente hablando.</p><p>Si posees esta vitamina en el inventario y alguien te tira una vitamina, el escudo se rompe y te salvará de la vitamina.</p><p>Si ya tienes escudos en el inventario y quieres proteger a alguien  puedes usar esta vitamina con alguien y la vitamina pasará al inventario de ese jugador</p>', '30', '3', 'Escudo.php');
